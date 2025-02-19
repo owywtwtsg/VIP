2a83cd3ae778b3f9cb91313c319dcf244d5a3f2f
owywtwtsg <testgrpmusi@gmail.com>
2025-02-19 23:58:47 +0530

    Update __main__.py

--- ChampuXMusic/__main__.py
+++ ChampuXMusic/__main__.py
@@ -22,7 +22,7 @@ async def init():
         and not config.STRING5
     ):
         LOGGER(__name__).error("ᴀssɪsᴛᴀɴᴛ ᴄʟɪᴇɴᴛ ᴠᴀʀɪᴀʙʟᴇs ɴᴏᴛ ᴅᴇғɪɴᴇᴅ, ᴇxɪᴛɪɴɢ...")
-        exit()
+        
     await sudo()
     try:
         users = await get_gbanned()
@@ -39,19 +39,9 @@ async def init():
     LOGGER("ChampuXMusic.plugins").info("sᴜᴄᴄᴇssғᴜʟʟʏ ɪᴍᴘᴏʀᴛᴇᴅ ᴍᴏᴅᴜʟᴇs...")
     await userbot.start()
     await Champu.start()
-    try:
-        await Champu.stream_call("https://telegra.ph/file/58cc6ef6d0a2a720ea6e3.mp4")
-    except NoActiveGroupCall:
-        LOGGER("ChampuXMusic").error(
-            "ᴘʟᴇᴀsᴇ ᴛᴜʀɴ ᴏɴ ᴛʜᴇ ᴠɪᴅᴇᴏᴄʜᴀᴛ ᴏғ ʏᴏᴜʀ ʟᴏɢ ɢʀᴏᴜᴘ\ᴄʜᴀɴɴᴇʟ.\n\nsᴛᴏᴘᴘɪɴɢ ʙᴏᴛ..."
-        )
-        exit()
-    except:
-        pass
+    
     await Champu.decorators()
-    LOGGER("ChampuXMusic").info(
-        "\x43\x68\x61\x6D\x70\x75\x20\x42\x6F\x74\x20\x68\x61\x73\x20\x62\x65\x65\x6E\x20\x73\x75\x63\x63\x65\x73\x73\x66\x75\x6C\x6C\x79\x20\x73\x74\x61\x72\x74\x65\x64\x2E\x0A\x0A\x40\x54\x68\x65\x43\x68\x61\x6D\x70\x75\x20"
-    )
+    
     await idle()
     await app.stop()
     await userbot.stop()
